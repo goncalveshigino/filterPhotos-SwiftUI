1f66433aea53d228d63fec9821caad946311b30a
goncalveshigino <goncalveshigino20@gmail.com>
2024-02-29 19:39:15 +0100

    add filter

---
+++ InstaFilter/ContentView.swift
@@ -0,0 +1,179 @@
+//
+//  ContentView.swift
+//  InstaFilter
+//
+//  Created by Goncalves Higino on 28/02/24.
+//
+import CoreImage
+import CoreImage.CIFilterBuiltins
+import PhotosUI
+import SwiftUI
+
+
+struct ContentView: View {
+    @State private var processedImage: Image?
+    @State private var filterIntensity = 0.5
+    @State private var selectedItem: PhotosPickerItem?
+    
+    @State private var currentFilter = CIFilter.sepiaTone()
+    let context = CIContext()
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Spacer()
+                
+                // image area
+                PhotosPicker(selection: $selectedItem){
+                    if let processedImage {
+                        processedImage
+                            .resizable()
+                            .scaledToFit()
+                    } else {
+                        ContentUnavailableView("No picture", systemImage: "photo.badge.plus", description: Text("Tap to import a photo"))
+                    }
+                }
+                .buttonStyle(.plain)
+                .onChange(of: selectedItem, loadImage)
+                
+                Spacer()
+                
+                HStack {
+                    Text("Intensity")
+                    Slider(value: $filterIntensity)
+                        .onChange(of:  filterIntensity, applyProcessing)
+                }
+                
+                HStack {
+                    Button("Change Filter", action: changeFilter)
+                    
+                    Spacer()
+                    
+                    //Share
+                }
+            }
+            .padding([.horizontal, .bottom])
+        .navigationTitle("Intafilter")
+        }
+    }
+    
+    func changeFilter() {
+        
+    }
+    
+    func loadImage() {
+        Task {
+            guard let imageData = try await selectedItem?.loadTransferable(type: Data.self) else { return }
+            guard let inputImage = UIImage(data: imageData) else { return }
+            
+            let beginImage = CIImage(image: inputImage)
+            currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+            applyProcessing()
+        }
+    }
+    
+    func applyProcessing() {
+        currentFilter.intensity = Float(filterIntensity)
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        processedImage = Image(uiImage: uiImage)
+    }
+}
+
+#Preview {
+    ContentView()
+}
+
+/*
+struct ContentView: View {
+    @State private var pickerItems = [PhotosPickerItem]()
+    @State private var selectedImages = [Image]()
+    
+    var body: some View {
+        VStack {
+            PhotosPicker("Select a picture", selection: $pickerItems,maxSelectionCount: 3, matching: .any(of: [.images, .not(.screenshots)]))
+               
+            ScrollView {
+                ForEach(0..<selectedImages.count, id: \.self) { image in
+                    selectedImages[image]
+                        .resizable()
+                        .scaledToFit()
+                }
+            }
+        }
+        .onChange(of: pickerItems) {
+            Task {
+                selectedImages.removeAll()
+                
+                for item in pickerItems {
+                    if let loadedImage = try await item.loadTransferable(type: Image.self) {
+                        selectedImages.append(loadedImage)
+                    }
+                }
+            }
+        }
+        //.onAppear(perform: loadImage)
+    }
+    
+   /* func loadImage() {
+        let inputImage = UIImage(resource: .luiz)
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.pixellate()
+        
+        currentFilter.inputImage = beginImage
+        let amount = 1.0
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        image = Image(uiImage: uiImage)
+    } */
+} */
+
+   /* func loadImage() {
+        let inputImage = UIImage(resource: .luiz)
+        let beginImage = CIImage(image: inputImage)
+        
+        let context = CIContext()
+        let currentFilter = CIFilter.pixellate()
+        
+        currentFilter.inputImage = beginImage
+        let amount = 1.0
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(amount, forKey: kCIInputIntensityKey) }
+        if inputKeys.contains(kCIInputRadiusKey) { currentFilter.setValue(amount * 200, forKey: kCIInputRadiusKey) }
+        if inputKeys.contains(kCIInputScaleKey) { currentFilter.setValue(amount * 10, forKey: kCIInputScaleKey) }
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        guard let cgImage = context.createCGImage(outputImage, from: outputImage.extent) else { return }
+        
+        let uiImage = UIImage(cgImage: cgImage)
+        image = Image(uiImage: uiImage)
+    }
+    
+    var body: some View {
+//        ShareLink(item: URL(string: "https://www.hackingwithswift.com")!, subject:
+//          Text("Lear Swift here"), message: Text("Check out rhe 100 Days of SwiftUI")
+//        ShareLink(item: URL(string: "https://www.hackingwithswift.com")!) {
+//            Label("Spread the word about Swift", systemImage: "swift")
+//        }
+        let example = Image(.luiz)
+        
+        ShareLink(item: example, preview: SharePreview("Singapore Airport", image: example)) {
+            Label("clik to shared", systemImage: "airplane")
+        }
+    }*/
---
+++ InstaFilter/InstaFilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstaFilterApp.swift
+//  InstaFilter
+//
+//  Created by Goncalves Higino on 28/02/24.
+//
+
+import SwiftUI
+
+@main
+struct InstaFilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
